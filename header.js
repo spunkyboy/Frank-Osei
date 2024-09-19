e38c46c3dde0f51b3ab7dde44ea7e8eff3bcd338
spunkyboy <120793467+spunkyboy@users.noreply.github.com>
2024-09-19 08:52:15 +0100

    Add files via upload

--- header.js
+++ header.js
@@ -23,7 +23,32 @@ function myPortfolios() {
    }
 
 window.onclick = function(event) {
-    if (!event.target.matches('.button_2')) {
+    if (!event.target.matches('#button_2')) {
         closeDropdown();
     }
 }
+
+
+const image = document.getElementById('personalImage');
+let isUp = false;
+
+image.onclick = function() {
+    if (isUp) {
+        image.style.transform = 'translateY(0)';
+    } else {
+        image.style.transform = 'translateY(-60px)'; 
+    }
+    isUp = !isUp; 
+};
+
+const element = document.getElementById('button_2');
+function updateContent() {
+    if (window.matchMedia("(max-width: 724px)").matches) {
+        element.innerHTML = "&#x2630";
+    } else {
+        element.innerHTML = '<span style="font-weight:bold;">F.O</span>';
+    }
+}
+
+updateContent();
+window.addEventListener('resize', updateContent);
